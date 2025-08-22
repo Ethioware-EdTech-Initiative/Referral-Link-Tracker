e46be909e37460180c33d98cbd0495e251ddec60
nahomer200 <nahumyne@ethioware.org>
2025-08-22 10:54:27 -0300

    created stats view and defined geo detail function

--- alxET-rt-api/dashboard_service/utils.py
+++ alxET-rt-api/dashboard_service/utils.py
@@ -4,8 +4,11 @@ import time
 from django.conf import settings
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
+
+IPINFO_URL = "https://ipinfo.io/{ip}/json"
 
 
 def generate_referral_code(campaign_id: str, officer_id: str, secret_key: str = None) -> str:
@@ -18,3 +21,24 @@ def generate_referral_code(campaign_id: str, officer_id: str, secret_key: str = 
 
     hash_digest = hmac.new(secret_key_bytes, message, hashlib.sha256).hexdigest()
     return hash_digest[:16]
+
+
+
+
+def get_geo_details(ip: str) -> dict:
+    if not ip:
+        return {"country": None, "city": None, "region": None}
+
+    try:
+        resp = requests.get(IPINFO_URL.format(ip=ip), timeout=3)
+        if resp.status_code == 200:
+            data = resp.json()
+            return {
+                "country": data.get("country"),
+                "city": data.get("city"),
+                "region": data.get("region"),
+            }
+        else:
+            return {"country": None, "city": None, "region": None}
+    except requests.RequestException:
+        return {"country": None, "city": None, "region": None}
