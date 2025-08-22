086c7ef3d9353cfc06e6cc190e826d1fb1968e79
nahomer200 <nahumyne@ethioware.org>
2025-08-22 14:00:18 -0300

    configured permission

--- alxET-rt-api/alx_recruitment_tracker/settings.py
+++ alxET-rt-api/alx_recruitment_tracker/settings.py
@@ -232,10 +232,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-     'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ],
-    
+
     # for fut config
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
