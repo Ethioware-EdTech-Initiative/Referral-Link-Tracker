3784ec0e55bc7f89e924406cbb0e36695f889c5d
Nahom Merga <nahumyne@ethioware.org>
2025-08-31 07:50:06 -0300

    Update build.sh

--- alxET-rt-api/build.sh
+++ alxET-rt-api/build.sh
@@ -9,18 +9,17 @@ python3 -m pip install --upgrade pip
 # Install dependencies
 pip3 install -r requirements.txt
 
-
 # Collect static files
 echo "Collecting static files..."
 python3 manage.py collectstatic --no-input
 
-# Run migrations
+# Run database migrations
 echo "Running migrations..."
 python3 manage.py migrate --no-input
 
 # Start Gunicorn server
 echo "Starting Gunicorn..."
-gunicorn alx_recruitment_tracker.wsgi:application \
+gunicorn aau_internB.wsgi:application \
     --bind 0.0.0.0:$PORT \
     --workers 3 \
     --timeout 120
